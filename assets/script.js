bfe8411af2ffa3b5d82b24cc1c90de105da0b878
Mike Sowers <msowers72@yahoo.com>
2021-07-01 15:10:34 -0400

    updated function that pulls in API KEY

--- assets/script.js
+++ assets/script.js
@@ -10,7 +10,7 @@ THEN I am presented with a 5-day forecast that displays the date, an icon repres
 WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city*/
 
-var key = "0c6c7c6766f0c82e84b46a07f2439d14";
+var API_Key = "0c6c7c6766f0c82e84b46a07f2439d14";
 var cityName = document.querySelector("#city-name");
 var searchBtn = document.querySelector(".btn");
 var clearHistory = document.querySelector("#clear-history");
@@ -20,8 +20,26 @@ var  temperature = document.querySelector('#temperature');
 
 var searchFormEl = document.querySelector('#city-history');
 var paragraph = document.querySelector('p');
+var timeEl = document.querySelector('.time')
+var dateEl = document.querySelector('.date');
+var timeZoneEl = document.querySelector('.time-zone');
 
 
+
+
+  setInterval(() => {
+   var time = new Date();
+   var month = time.getMonth();
+   var date = time.getDate();
+   var day = time.getDay();
+   var hour = time.getHours();
+   var hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+   var minutes = time.getMinutes();
+   var ampm = hour >=12 ? 'PM' : 'AM'
+
+     timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes+ ' ' + `<span id="am-pm">${ampm}</span>`
+
+  }, 1000);
 
 
 
@@ -35,39 +53,54 @@ function renderButton(e) {
     
 }
 
-function getApi() {
-    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName.value + "&appid=" + key;
+getWeatherData();
+function getWeatherData () {
+  navigator.geolocation.getCurrentPosition((success) => {
+    console.log(success);
 
-  console.log(cityName.value)
+    var {latitude, longitude } = success.coords;
+
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${API_Key}`).then(res => res.json()).then(data => {
+
+      console.log(data);     
+    })
+   })
+}
+
+
+// function getApi() {
+//     var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName.value + "&appid=" + key;
+
+//   console.log(cityName.value)
 
   
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        //Using console.log to examine the data
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-          //Creating a h3 element and a p element
-          var userName = document.createElement('h3');
-          var userUrl = document.createElement('p');
+//     fetch(requestUrl)
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (data) {
+//         //Using console.log to examine the data
+//         console.log(data);
+//         for (var i = 0; i < data.length; i++) {
+//           //Creating a h3 element and a p element
+//           var userName = document.createElement('h3');
+//           var userUrl = document.createElement('p');
   
-          //Setting the text of the h3 element and p element.
-          userName.textContent = data[i].login;
-          userUrl.textContent = data[i].url;
+//           //Setting the text of the h3 element and p element.
+//           userName.textContent = data[i].login;
+//           userUrl.textContent = data[i].url;
   
-          //Appending the dynamically generated html to the div associated with the id="users"
-          //Append will attach the element as the bottom most child.
-          cityName.append(userName);
-          temperature.append(userUrl);
-        }
-      });
-  }
+//           //Appending the dynamically generated html to the div associated with the id="users"
+//           //Append will attach the element as the bottom most child.
+//           cityName.append(userName);
+//           temperature.append(userUrl);
+//         }
+//       });
+  // }
 
 
 
 
 
-  searchBtn.addEventListener('click', getApi);
+  searchBtn.addEventListener('click', getWeatherData);
   searchBtn.addEventListener('click', renderButton);
