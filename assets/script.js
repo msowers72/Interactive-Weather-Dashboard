117fa481609694768686ef6884932ec7d9a6e731
Mike Sowers <msowers72@yahoo.com>
2021-07-02 09:31:36 -0400

    added info to js page fixed time function

--- assets/script.js
+++ assets/script.js
@@ -16,8 +16,11 @@ var searchBtn = document.querySelector(".btn");
 var clearHistory = document.querySelector("#clear-history");
 var cityHistory = document.querySelector("#city-history");
 var formEl = document.querySelector('form');
-var  temperature = document.querySelector('#temperature');
-
+var temperature = document.querySelector('#temperature');
+var humidity = document.querySelector('#humidity');
+var windSpeed = document.querySelector('#wind-speed');
+var uvIndex = document.querySelector('#uv-index');
+var temp = document.querySelector('#temp');
 var searchFormEl = document.querySelector('#city-history');
 var paragraph = document.querySelector('p');
 var timeEl = document.querySelector('.time')
@@ -28,59 +31,59 @@ var todayWeather = document.querySelector('#today-weather')
 
 
 
-  setInterval(() => {
-   var time = new Date();
-   var month = time.getMonth();
-   var date = time.getDate();
-   var day = time.getDay();
-   var hour = time.getHours();
-   var hoursIn12HrFormat = hour >= 13 ? hour %12: hour
-   var minutes = time.getMinutes();
-   var ampm = hour >=12 ? 'PM' : 'AM'
+setInterval(() => {
+  var time = new Date();
+  var month = time.getMonth();
+  var date = time.getDate();
+  var day = time.getDay();
+  var hour = time.getHours();
+  var hoursIn12HrFormat = hour >= 13 ? hour % 12 : hour
+  var minutes = time.getMinutes();
+  var ampm = hour >= 12 ? 'PM' : 'AM'
 
-     timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes+ ' ' + `<span id="am-pm">${ampm}</span>`
+  timeEl.innerHTML = (hoursIn12HrFormat < 10? '0' +hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0' +minutes: minutes) + ' ' + `<span id="am-pm">${ampm}</span>`
 
-  }, 1000);
+}, 1000);
 
 
 
 
 
 function renderButton(e) {
-    e.preventDefault();   
-    console.log("I'm working")
+  e.preventDefault();
+  console.log("I'm working")
 
-    
-    
+
+
 }
 
 getWeatherData();
-function getWeatherData () {
+function getWeatherData() {
   navigator.geolocation.getCurrentPosition((success) => {
     console.log(success);
 
-    var {latitude, longitude } = success.coords;
+    var { latitude, longitude } = success.coords;
 
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_Key}`).then(res => res.json()).then(data => {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_Key}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
 
-      console.log(data); 
-      
-      displayWeatherData(data);
-    })
-   })
+        displayWeatherData(data);
+      })
+  })
 }
 
-function displayWeatherData (data){
-var [temperature, humidity, wind_speed, uvi] = data.current;
+function displayWeatherData(data) {
+  console.log("in displayWeatherData")
+  console.log(data)
+  var { current: { temp, humidity, wind_speed, uvi, timezone } } = data;
+  temperature.innerHTML = temp;
+  cityName.innerHTML = timezone; 
+  humidity.innerHTML = humidity;
 
-todayWeather.innerHTML =
-`<div id="today-weather">
-    <div> id="temperature">Temperature:</div>
-    <div>${temperature}</div>
-    <p id="humidity"> Humidity:</p>
-    <p id="wind-speed">Wind Speed:</p>
-    <p id="UV-index"> UV Index:</p>
-</div>`;
+
+  
 
 }
 
@@ -90,7 +93,7 @@ todayWeather.innerHTML =
 
 //   console.log(cityName.value)
 
-  
+
 //     fetch(requestUrl)
 //       .then(function (response) {
 //         return response.json();
@@ -102,22 +105,22 @@ todayWeather.innerHTML =
 //           //Creating a h3 element and a p element
 //           var userName = document.createElement('h3');
 //           var userUrl = document.createElement('p');
-  
+
 //           //Setting the text of the h3 element and p element.
 //           userName.textContent = data[i].login;
 //           userUrl.textContent = data[i].url;
-  
+
 //           //Appending the dynamically generated html to the div associated with the id="users"
 //           //Append will attach the element as the bottom most child.
 //           cityName.append(userName);
 //           temperature.append(userUrl);
 //         }
 //       });
-  // }
+// }
 
 
 
 
 
-  searchBtn.addEventListener('click', getWeatherData);
-  searchBtn.addEventListener('click', renderButton);
+searchBtn.addEventListener('click', getWeatherData);
+searchBtn.addEventListener('click', renderButton);
